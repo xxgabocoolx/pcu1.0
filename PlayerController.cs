788c3e722043f4e41ec889d275ad3a4e37c05e0f
gabo <gegm2006@gmail.com>
2020-04-26 16:48:54 -0500

    PlayerController 1.0

---
+++ PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalMove;
+    public float verticalMove;
+    private Vector3 playerInput;
+
+    public CharacterController Player;
+
+    public float PlayerSpeed;
+    private Vector3 movePlayer;
+    public float gravity = 9.8f;
+
+
+    public Camera mainCamera;
+    private Vector3 camForward;
+    private Vector3 camRight;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxis("Horizontal");
+        verticalMove = Input.GetAxis("Vertical");
+
+        playerInput = new Vector3(horizontalMove, 0, verticalMove);
+        playerInput = Vector3.ClampMagnitude(playerInput, 1);
+
+        camDirection();
+
+        movePlayer = playerInput.x * camRight + playerInput.z * camForward;
+
+        movePlayer = movePlayer * PlayerSpeed;
+
+        Player.transform.LookAt(Player.transform.position + movePlayer);
+
+        setGravity();
+
+        Player.Move(movePlayer * Time.deltaTime);
+
+        Debug.Log(Player.velocity.magnitude);
+    }
+
+    void camDirection()
+    {
+        camForward = mainCamera.transform.forward;
+        camRight = mainCamera.transform.right;
+
+        camForward.y = 0;
+        camRight.y = 0;
+
+        camForward = camForward.normalized;
+        camRight = camRight.normalized;
+
+    }
+
+    void setGravity()
+    {
+        movePlayer.y = -gravity * Time.deltaTime;
+    }
+
+
+} 
